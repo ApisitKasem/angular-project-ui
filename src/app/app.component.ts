b442ceb9cfb2d912d7f3ed5ad0139e56bc69b538
ApisitKasem <mr.apisit.kasem@gmail.com>
2025-05-15 14:23:30 +0700

    update ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SideMenuComponent} from './components/side-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, SideMenuComponent],
+  template: `
+    <div style="display: flex; height: 100vh">
+      <app-side-menu style="width: 250px;"></app-side-menu>
+      <div style="flex: 1; padding: 16px;">
+        <router-outlet />
+      </div>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'angular-project-ui';
-}
+export class AppComponent {}
